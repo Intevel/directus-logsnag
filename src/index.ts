83be712d3ce4c666076a56b5e6c0371d593d36ab
Conner Bachmann <59223342+Intevel@users.noreply.github.com>
2022-07-07 20:50:23 +0200

    feat(chore): implemented logsnag on user registrations

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,35 @@
-import { defineHook } from '@directus/extensions-sdk';
+import fetch from "node-fetch";
 
-export default defineHook(({ filter, action }) => {
-	filter('items.create', () => {
-		console.log('Creating Item!');
-	});
+// @ts-ignore
+export default ({ action }, { services, exceptions, database: knex }) => {
+	const { ItemsService } = services;
 
-	action('items.create', () => {
-		console.log('Item created!');
+	// @ts-ignore
+	action("users.create", async ({ collection, payload }, { schema }) => {
+		const { ServiceUnavailableException } = exceptions;
+		const itemService = new ItemsService("logsnag", { knex, schema, accountability: { admin: true, ip: "127.0.0.1" } });
+		const settings = await itemService.readSingleton({ fields: ["*"] });
+		// if (!settings.project || !settings.channel || !settings.title || !settings.description || settings.emoji) return;
+		try {
+			const headers = {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${settings.api_token}`,
+			};
+			var response = await fetch("https://api.logsnag.com/v1/log", {
+				method: "POST",
+				headers,
+				body: JSON.stringify({
+					project: settings.project,
+					channel: settings.channel,
+					event: settings.message_title,
+					description: settings.message_description,
+					icon: settings.emoji,
+					notify: true,
+				}),
+			});
+			console.log(response);
+		} catch (error) {
+			throw new ServiceUnavailableException(error);
+		}
 	});
-});
+};
